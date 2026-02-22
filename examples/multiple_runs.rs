d87a6a34bf575005b12ce7e6c0bb3989e0befda1
Loric ANDRE <loric.andre@pm.me>
2026-02-22 16:51:56 +0100

    fix: make sure we drop Skim before returning the output

--- examples/multiple_runs.rs
+++ examples/multiple_runs.rs
@@ -1,3 +1,4 @@
+use nix::libc::malloc_trim;
 use skim::{Skim, prelude::SkimOptionsBuilder};
 
 // Hint: use `ps -T -p $(pgrep -f target/debug/examples/multiple_runs)` to watch threads while the
@@ -6,9 +7,13 @@ fn main() {
     for i in 0..3 {
         let opts = SkimOptionsBuilder::default()
             .header(format!("run {i}"))
+            .cmd("cat benches/fixtures/10M.txt")
             .build()
             .unwrap();
         let res = Skim::run_with(opts, None).unwrap();
+        unsafe {
+            malloc_trim(0);
+        }
         println!(
             "run {i}: {:?}, sleeping for 5 secs",
             res.selected_items.first().map(|x| x.output())
